1c29b3b7def79a7c6d17e05aa06fd0ab4dc5fe8e
Justin Kelly <justin@kelly.org.au>
2021-09-12 21:49:02 +1000

    bootstrap padding

--- netlify/data-html.go
+++ netlify/data-html.go
@@ -27,7 +27,7 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
         //blah := string(data)
        // value := gjson.Get(string(data), "item_description.0.value")
         var blah string
-        blah = "<html><head><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'></head><body><table class='table table-striped table-bordered table-sm table'>"
+        blah = "<html><head><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'></head><body style='padding:15px;'><table class='table table-striped table-bordered table-sm table'>"
         blah = blah + "<thead>"
         blah = blah + "<tr><th colspan='2'>API data:</th></tr>"
         blah = blah + "</thead><tbody>"
