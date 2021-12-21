121515e45461869ba1e77f8a317733199aad92a7
Justin Kelly <justin@kelly.org.au>
2021-12-21 14:53:01 +1100

    json update - model

--- netlify/data-json.go
+++ netlify/data-json.go
@@ -17,8 +17,15 @@ func json_handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProx
 	//uuid := request.PathParameters["uuid"]
 	metadata_key := request.QueryStringParameters["metadata_key"]
 	metadata_value := request.QueryStringParameters["metadata_value"]
+        metadata_model := request.QueryStringParameters["metadata_model"]
 
-	data_id := Request("https://hgrqtmovrbvpvxjqungh.supabase.co/rest/v1/item_metadata?metadata_key=eq." + metadata_key + "&metadata_value=eq." + metadata_value)
+        var data_id []byte
+        if len(metadata_model) == 0 {
+          data_id = Request("https://hgrqtmovrbvpvxjqungh.supabase.co/rest/v1/item_metadata_transpose?"+metadata_key+"=eq."+metadata_value+"&order=model.desc.nullslast&limit=1")
+        } else {
+          data_id = Request("https://hgrqtmovrbvpvxjqungh.supabase.co/rest/v1/item_metadata_transpose?"+metadata_key+"=eq."+metadata_value+"&and=(model.eq."+metadata_model+")&limit=1")
+        }
+
 	request_data_id := gjson.Get(string(data_id), "item_id").String()
 
 	data := Request("https://hgrqtmovrbvpvxjqungh.supabase.co/rest/v1/item?select=id,url,item_brand(value,score,x,y,width,height),item_category(value,score),item_celebrity(value,score,position_height,position_left,position_top,position_width),item_color(black_and_white,accent_color,dominant_color_background,dominant_color_foreground,dominant_colors),item_description(value,score),item_face(position_height,position_left,position_top,position_width),item_landmark(value,score),item_object(value,score,x,y,width,height),item_tag(value,score),item_text(line,value,box),item_text_entity(value,match_text,text_type,text_sub_type,text_score),item_text_key_phrase(value),item_text_language(value,code,score),item_text_sentiment(score),item_metadata(metadata_key,metadata_value),item_log(section,value)&and=(id.eq." + request_data_id + ")")
